80e411637c509716a377a497d7ca99a1bc0e74af
Déborah Scheuren <110463007+scheudeb@users.noreply.github.com>
2023-09-23 10:42:42 +0200

    avancees

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,89 @@
-import './App.css'
-import DepenseForm from './component/DepenseForm'
-import { useReducer } from 'react'
+import React, { useReducer, useEffect } from 'react';
+import './App.css';
+import DepenseForm from './component/DepenseForm';
 
-const initialState={
-  depenses:[],
-  total:0
-}
-const reducer=(state,action)=>{
-  switch(action.type){
-    case "add":
+const initialState = {
+  depenses: [],
+  total: 0,
+  categorie: {
+    sport: 0,
+    alimentation: 0,
+    loisirs: 0,
+    logement: 0,
+    transport: 0,
+    santé: 0,
+    autres: 0,
+  },
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'add':
+      const updatedCategorie = { ...state.categorie };
+      updatedCategorie[action.payload.categorie] += (
+        isNaN(action.payload.prix) ? 0 : parseInt(action.payload.prix)
+      );
+
+      const updatedTotal = state.total + (
+        isNaN(action.payload.prix) ? 0 : parseInt(action.payload.prix)
+      );
+
       return {
         ...state,
-        depenses:[...state.depenses,action.payload],
-        total:state.total + Number(action.payload.prix)
-      }
-      
+        depenses: [...state.depenses, action.payload],
+        total: updatedTotal,
+        categorie: updatedCategorie,
+      };
+
+    default:
+      return state;
   }
-}
+};
+
+
+
 function App() {
-  const [state,dispatch]=useReducer(reducer,initialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // Calculer le total pour chaque catégorie une seule fois, lorsque le composant App est monté.
+  useEffect(() => {
+    const calculateTotalByCategory = () => {
+      const updatedCategorie = { ...state.categorie };
+      Object.entries(updatedCategorie).forEach(([catName, catTotal]) => {
+        updatedCategorie[catName] = state.depenses
+          .filter((dep) => dep.categorie === catName)
+          .reduce((acc, dep) => acc + dep.prix, 0);
+      });
+
+      dispatch({ type: 'update', payload: { categorie: updatedCategorie } });
+    };
+
+    calculateTotalByCategory();
+  }, [state.depenses]);
 
   return (
     <>
-    <DepenseForm dispatch={dispatch} />
-    <h1>Total {state.total}$ </h1>
-    {state.depenses.map((dep)=>{
-      return (<div> 
-        <h1>  {dep.categorie} </h1>
-        <h2> {dep.description} </h2>
-        <h3> {dep.prix} </h3>
-         </div>)
-    }
-     )}
+      <DepenseForm dispatch={dispatch} />
+      <h1>Total général: {state.total}$</h1>
+      <h2>Total par catégorie:</h2>
+      <ul>
+        {Object.entries(state.categorie).map(([catName, catTotal]) => (
+          <li key={catName}>
+            {catName}: {catTotal}$
+          </li>
+        ))}
+      </ul>
+      {state.depenses.map((dep, index) => (
+        <div key={index}>
+          <h1>{dep.categorie}</h1>
+          <h2>{dep.description}</h2>
+          <h3>{dep.prix}$</h3>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+
+
+export default App;
